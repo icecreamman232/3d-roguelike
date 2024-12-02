78dd4a88b01ffb2bdee3e4fab74b5fa5b94f0729
Stephen Ho <stephen.ho.232@gmail.com>
2024-12-02 15:09:21 +0700

    Refactor pickable class

--- Assets/_Scripts/Pickables/Coin.cs
+++ Assets/_Scripts/Pickables/Coin.cs
@@ -1,55 +1,17 @@
-using System.Collections;
 using SGGames.Scripts.Data;
 using UnityEngine;
 
 namespace SGGames.Scripts.Pickables
 {
-    public class Coin : MonoBehaviour, Pickable
+    public class Coin : Pickable
     {
-        [SerializeField] private float m_flySpeed;
         [SerializeField] private int m_coinValue;
         [SerializeField] private PickablePickedEvent m_pickedEvent;
-        private bool m_hasStartedPicking;
-        private bool m_canFly;
-        private Transform m_playerTransform;
-        private float m_delayBeforeFly = 0.5f;
-    
-        private void OnEnable()
-        {
-            m_hasStartedPicking = false;
-        }
-    
-        public void StartPick(Transform target)
-        {
-            if (m_hasStartedPicking) return;
-            m_hasStartedPicking = true;
-            m_playerTransform = target;
-            StartCoroutine(DelayBeforeFly());
-        }
-    
-        public void Picked()
+  
+        public override void Picked()
         {
             m_pickedEvent.Raise(PickableType.Coin,m_coinValue);
             Destroy(this.gameObject);
-        }
-    
-        private IEnumerator DelayBeforeFly()
-        {
-            m_canFly = false;
-            yield return new WaitForSeconds(m_delayBeforeFly);
-            m_canFly = true;
-        }
-    
-        private void Update()
-        {
-            if (!m_canFly) return;
-            if (m_playerTransform == null) return;
-            transform.position = Vector3.MoveTowards(transform.position,m_playerTransform.position , m_flySpeed * Time.deltaTime);
-    
-            if (Vector3.Distance(transform.position, m_playerTransform.position) < 0.5f)
-            {
-                Picked();
-            }
         }
     }
 }
--- Assets/_Scripts/Pickables/Pickable.cs
+++ Assets/_Scripts/Pickables/Pickable.cs
@@ -1,4 +1,4 @@
-
+using System.Collections;
 using UnityEngine;
 
 namespace SGGames.Scripts.Pickables
@@ -10,10 +10,46 @@ namespace SGGames.Scripts.Pickables
         Health_Potion,
     }
     
-    public interface Pickable
+    public class Pickable : MonoBehaviour
     {
-        public void StartPick(Transform target) { }
-        public void Picked() { }
+        [SerializeField] private float m_flySpeed;
+        protected bool m_hasStartedPicking;
+        protected bool m_canFly;
+        protected Transform m_playerTransform;
+        protected float m_delayBeforeFly = 0.5f;
+        
+        protected void OnEnable()
+        {
+            m_hasStartedPicking = false;
+        }
+
+        public virtual void StartPick(Transform target)
+        {
+            if (m_hasStartedPicking) return;
+            m_hasStartedPicking = true;
+            m_playerTransform = target;
+            StartCoroutine(DelayBeforeFly());
+        }
+        public virtual void Picked() { }
+        
+        protected virtual void Update()
+        {
+            if (!m_canFly) return;
+            if (m_playerTransform == null) return;
+            transform.position = Vector3.MoveTowards(transform.position,m_playerTransform.position , m_flySpeed * Time.deltaTime);
+    
+            if (Vector3.Distance(transform.position, m_playerTransform.position) < 0.5f)
+            {
+                Picked();
+            }
+        }
+        
+        protected virtual IEnumerator DelayBeforeFly()
+        {
+            m_canFly = false;
+            yield return new WaitForSeconds(m_delayBeforeFly);
+            m_canFly = true;
+        }
     }
 }
 
